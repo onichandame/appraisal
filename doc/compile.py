93d38ef257a9fabae68b422e78722a81600e7716
onichandame <zxinmyth@gmail.com>
2019-12-21 14:19:34 +0800

    init

---
+++ doc/compile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This script will call latexmk --xelatex to create main.pdf and move it the the parent directory
+"""
+import subprocess
+command='latexmk --xelatex main.tex -silent'
+subprocess.run(command.split())
+import os
+_cwd_=os.getcwd()
+if os.path.isfile('main.pdf'):
+    command='mv '+os.path.join(_cwd_,'main.pdf')+' '+os.path.join(_cwd_,os.path.basename(_cwd_)+'.pdf')
+    subprocess.run(command.split())
+command ='latexmk -c'
+subprocess.run(command.split())
